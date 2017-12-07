d72da8dd440bf49cdef8e51c6f153f6360083d16
wilson.alencar <wilsonalencar@movida.com.br>
2017-12-07 21:53:34 -0200

    retirando database.php

--- model/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli_connection = new MySQLi('127.0.0.1', 'root', 'cpd123');
-if($mysqli_connection->connect_error){
-   echo "Desconectado! Erro: " . $mysqli_connection->connect_error;
-}
-?>
