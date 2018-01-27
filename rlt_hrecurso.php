31a6386de59e0c5467c7ed821b664343d99ac9bc
root <lucas.saboreweb@gmail.com>
2018-01-27 11:44:21 -0200

    datatables

--- rlt_hrecurso.php
+++ rlt_hrecurso.php
@@ -28,7 +28,7 @@
                         </div>
                         <div class="card-content">
                             <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" >
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                                     <thead>
                                         <tr>
                                             <th>Pilar</th>
