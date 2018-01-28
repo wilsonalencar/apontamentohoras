0de4b7dbb4cc47a7547622ad152e761d52037005
root <lucas.saboreweb@gmail.com>
2018-01-28 00:27:16 -0200

    Corrigindo exibição do código

--- model/proposta.php
+++ model/proposta.php
@@ -123,13 +123,13 @@ class proposta extends app
 	public function montaSelect($selected=0)
 	{
 		$conn = $this->getDB->mysqli_connection;
-		$query = sprintf("SELECT id,nome FROM propostas WHERE status = '%s' ORDER BY nome", $this::STATUS_SISTEMA_ATIVO);
+		$query = sprintf("SELECT id,codigo FROM propostas WHERE status = '%s' ORDER BY nome", $this::STATUS_SISTEMA_ATIVO);
 
 		if($result = $conn->query($query))
 		{
 			while($row = $result->fetch_array(MYSQLI_ASSOC))
 			echo sprintf("<option %s value='%d'>%s</option>\n", $selected == $row['id'] ? "selected" : "",
-			$row['id'], $row['nome']);
+			$row['id'], $row['codigo']);
 		}
 	}
 
