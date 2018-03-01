c08bd47d86ef01deb2078c5834fd93b03b4cf8b6
root <lucas.saboreweb@gmail.com>
2018-03-01 09:26:35 -0300

    debugando

--- model/PHPMailer/PHPMailer.php
+++ model/PHPMailer/PHPMailer.php
@@ -285,7 +285,13 @@ class PHPMailer
      *
      * @var array
      */
-    public $SMTPOptions = [];
+    public $SMTPOptions = array(
+                    'ssl' => array(
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true
+                    )
+                );
 
     /**
      * SMTP username.
