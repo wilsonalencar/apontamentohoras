517082041ec6443c91175cd29ff48acd94951503
root <lucas.saboreweb@gmail.com>
2018-09-28 19:03:32 -0300

    melhoria

--- consulta_projetos.php
+++ consulta_projetos.php
@@ -93,25 +93,25 @@ $('#dataTables-example').dataTable({
              {
                 extend: 'copyHtml5',
                 exportOptions: {
-                   columns: [ 0, 1, 2, 3, 4]
+                   columns: [ 0, 1, 2, 3, 4, 5]
                 }
              },
              {
                 extend: 'excelHtml5',
                 exportOptions: {
-                   columns: [0, 1, 2, 3, 4]
+                   columns: [0, 1, 2, 3, 4, 5]
                 }
              },
              {
                 extend: 'csvHtml5',
                 exportOptions: {
-                   columns: [0, 1, 2, 3, 4]
+                   columns: [0, 1, 2, 3, 4, 5]
                 }
              },
              {
                 extend: 'pdfHtml5',
                 exportOptions: {
-                   columns: [0, 1, 2, 3, 4]
+                   columns: [0, 1, 2, 3, 4, 5]
                 }
              },
          ]
